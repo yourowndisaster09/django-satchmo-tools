ec1f73ce24107095afdb3129c311bde94cb5b754
ubi <yourowndisaster0@gmail.com>
2013-11-14 11:25:12 +0800

    Add override option when creating store config

--- satchmo_tools/store/management/commands/createstore.py
+++ satchmo_tools/store/management/commands/createstore.py
@@ -1,3 +1,5 @@
+from optparse import make_option
+
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.core.management.base import BaseCommand, CommandError
@@ -8,26 +10,51 @@ from satchmo_store.shop.models import Config
 
 class Command(BaseCommand):
     help = 'Creates Store Configuration'
+    base_options = (
+        make_option('--override', action='store_true', dest='override', default=False,
+            help='Override existing store configuration'
+        ),
+    )
+    option_list = BaseCommand.option_list + base_options
 
     def __init__(self):
         super(Command, self).__init__()
-        self.check_settings()
+        self.set_settings()
         self.site = self.create_site_object()
 
     def handle(self, *args, **options):
-        self.create_store_config()
-        self.stdout.write('Completed creating store configuration\n')
-
-    def check_settings(self):
+        create_new = False
         try:
-            self.ENV_SITE_NAME = settings.ENV_SITE_NAME
-        except AttributeError:
-            raise CommandError('Add `ENV_SITE_NAME` to settings')
+            Config.objects.get(site=self.site)
+        except Config.DoesNotExist:
+            create_new = True
+            print("No existing configuration")
 
+        if options.get('override'):
+            create_new = True
+            print("Overriding existing store configuration")
+
+        if create_new:
+            self.create_store_config()
+            self.stdout.write('Completed creating store configuration\n')
+        else:
+            self.stdout.write('Already has store configuration\n')
+
+    def _set_settings(self, name):
         try:
-            self.SHOP_NAME = settings.SHOP_NAME
+            setting_value = getattr(settings, name)
+            setattr(self, name, setting_value)
         except AttributeError:
-            raise CommandError('Add `SHOP_NAME` to settings')
+            raise CommandError('Add `%s` to settings' % name)
+
+    def set_settings(self):
+        required_settings = [
+            'ENV_SITE_NAME',
+            'SHOP_NAME',
+            'COUNTRY_NAME'
+        ]
+        for s in required_settings:
+            self._set_settings(s)
 
     def create_site_object(self):
         try:
@@ -44,21 +71,22 @@ class Command(BaseCommand):
 
     def create_store_config(self):
         try:
-            usa_country = Country.objects.get(name="UNITED STATES")
+            country = Country.objects.get(name=self.COUNTRY_NAME)
         except Country.DoesNotExist:
-            raise CommandError('Run `python manage.py satchmo_load_l10n` first')
+            raise CommandError('You must run `python manage.py satchmo_load_l10n`\
+                first or enter a valid country')
 
         try:
             config = Config.objects.get(site=self.site)
             config.store_name = self.SHOP_NAME
-            config.country = usa_country
-            config.sales_country = usa_country
+            config.country = country
+            config.sales_country = country
             config.save()
         except Config.DoesNotExist:
             config = Config.objects.create(
                 site=self.site,
                 store_name=self.SHOP_NAME,
-                country=usa_country,
-                sales_country=usa_country
+                country=country,
+                sales_country=country
             )
-        config.shipping_countries.add(usa_country)
+        config.shipping_countries.add(country)
