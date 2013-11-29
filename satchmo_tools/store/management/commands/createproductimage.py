51317b83bb7eaf3f8465a9b6709eb46b9bceb371
ubi <yourowndisaster0@gmail.com>
2013-11-29 16:30:40 +0800

    Fix upload default_product_image

--- satchmo_tools/store/management/commands/createproductimage.py
+++ satchmo_tools/store/management/commands/createproductimage.py
@@ -1,42 +1,38 @@
-import os
 import urllib
+from optparse import make_option
 
 from django.contrib.staticfiles import finders
 from django.core.files import File
+from django.core.files.storage import default_storage
 from django.core.management.base import BaseCommand
-
-from product.models import CategoryImage, ProductImage
 
 from ....config import get_satchmo_tools_setting
 
 
 class Command(BaseCommand):
     help = 'Uploads default unavailable product picture'
+    base_options = (
+        make_option('--override', action='store_true', dest='override', default=False,
+            help='Override existing unavailable product picture'
+        ),
+    )
+    option_list = BaseCommand.option_list + base_options
 
     def handle(self, *args, **options):
-        try:
-            product_image = ProductImage.objects.get(pk=1)
-        except ProductImage.DoesNotExist:
-            product_image = ProductImage()
-
-        try:
-            category_image = CategoryImage.objects.get(pk=1)
-        except CategoryImage.DoesNotExist:
-            category_image = CategoryImage()
-
-        self._save_picture(product_image)
-        self._save_picture(category_image)
-
-        self.stdout.write('Added unavailable product picture\n')
-
-    def _save_picture(self, image):
         product_image = get_satchmo_tools_setting('DEFAULT_PRODUCT_IMAGE')
         file_path = finders.find(product_image)
+
         result = urllib.urlretrieve(file_path)
-        if hasattr(image, 'picture') and image.picture:
-            image.picture.delete()
-        image.picture.save(
-            os.path.basename(file_path),
-            File(open(result[0]))
-        )
-        image.save()
+        content = File(open(result[0]))
+
+        filenames = [
+            'images/productimage-picture-default.jpg',
+            'images/categoryimage-picture-default.jpg'
+        ]
+
+        for filename in filenames:
+            if not default_storage.exists(filename):
+                default_storage.save(filename, content)
+            elif options.get('override'):
+                default_storage.delete(filename)
+                default_storage.save(filename, content)
